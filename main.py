3645f9a940605128089cee976b8f979379ac5690
Niustein <sniuwork@gmail.com>
2025-01-13 16:02:27 -0700

    add 60 fps

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    py_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+#        pygame.Surface.fill(screen, (0,0,0)) - works, was my solution, swapping 
+        screen.fill("black")
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
+        dt = py_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
